ff708575d50bd356d49d4d26441a39a7dd9585c0
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-10-25 10:42:36 +0200

    need to get click & pressed to function

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-// const buttonEl = document.querySelectorAll(`button`);
+`use strict`;
 const drumEl = document.querySelectorAll(`.drum`);
+const buttonEl = document.querySelectorAll(`button`);
 
 for (let i = 0; i < drumEl.length; i++) {
-    drumEl[i].addEventListener('click', function () {
-        let buttons = this.innerHTML;
-
-        switch (buttons) {
-            case `a`:
-                let tomOne = new Audio(`sounds/tom-1.mp3`);
-                tomOne.play();
-                break;
-
-            case `s`:
-                let crash = new Audio(`sounds/crash.mp3`);
-                crash.play();
-                break;
-
-            case `d`:
-                let tomTwo = new Audio(`sounds/tom-2.mp3`);
-                tomTwo.play();
-                break;
-
-            case `f`:
-                let kickBass = new Audio(`sounds/kick-bass.mp3`);
-                kickBass.play();
-                break;
-
-            case `g`:
-                let tomThree = new Audio(`sounds/tom-3.mp3`);
-                tomThree.play();
-                break;
-
-            case `h`:
-                let snare = new Audio(`sounds/snare.mp3`);
-                snare.play();
-                break;
-
-            case `j`:
-                let tomFour = new Audio(`sounds/tom-4.mp3`);
-                tomFour.play();
-                break;
-
-            default:
-                console.log(buttons);
-        }
+    drumEl[i].addEventListener(`click`, function () {
+        let btns = this.innerHTML;
+        sound(btns);
+        animation(btns);
     });
 }
 
-// let tom = new Audio(`sounds/tom-1.mp3`);
-// tom.play();
-// this.style.color = `white`;
+document.addEventListener(`keydown`, function (event) {
+    sound(event.key);
+    animation(event.key);
+});
+
+const animation = function () {
+    buttonEl.classList.add(`pressed`);
+
+    setTimeout(function () {
+        buttonEl.classList.remove(`pressed`);
+    }, 1000);
+};
+
+const sound = function (key) {
+    switch (key) {
+        case `a`:
+            const tomOne = new Audio(`sounds/tom-1.mp3`);
+            tomOne.play();
+            break;
+
+        case `s`:
+            const crash = new Audio(`sounds/crash.mp3`);
+            crash.play();
+            break;
+
+        case `d`:
+            const tomTwo = new Audio(`sounds/tom-2.mp3`);
+            tomTwo.play();
+            break;
+
+        case `f`:
+            const kickBass = new Audio(`sounds/kick-bass.mp3`);
+            kickBass.play();
+            break;
+
+        case `g`:
+            const tomThree = new Audio(`sounds/tom-3.mp3`);
+            tomThree.play();
+            break;
+
+        case `h`:
+            const snare = new Audio(`sounds/snare.mp3`);
+            snare.play();
+            break;
+
+        case `j`:
+            const tomFour = new Audio(`sounds/tom-4.mp3`);
+            tomFour.play();
+            break;
+
+        default:
+            return key;
+    }
+};
